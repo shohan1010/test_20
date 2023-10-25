4af0f2667112a50a5a8f1454c0ae493281082a81
Md Sabbir Roshid Shohan <sabbirshohan80@gmail.com>
2023-10-26 02:24:45 +0600

    sd
    asd

---
+++ src/Component/Maybe_later.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo from './logo.svg';
+
+
+const Maybe_later = () => {
+  return (
+    <div>
+        <header className="App-header">
+        
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      
+    </div>
+  );
+};
+
+export default Maybe_later;
